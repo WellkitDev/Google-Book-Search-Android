5ba70ca29c95af621445dcf01410be83de85dbe5
WellkitDp <82035540+WellkitDP@users.noreply.github.com>
2022-11-25 22:41:05 +0700

    Create connectivity_service.dart

---
+++ book_search_api/lib/services/connectivity_service.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:book_search_api/services/connectivity_status.dart';
+
+class ConnectivityService {
+  StreamController<ConnectivityStatus> connectionStatusController =
+      StreamController<ConnectivityStatus>();
+
+  ConnectivityService() {
+    Connectivity().onConnectivityChanged.listen((ConnectivityResult result) {
+      connectionStatusController.add(_getStatusFromResult(result));
+    });
+  }
+  ConnectivityStatus _getStatusFromResult(ConnectivityResult result) {
+    switch (result) {
+      case ConnectivityResult.mobile:
+        return ConnectivityStatus.Callular;
+      case ConnectivityResult.wifi:
+        return ConnectivityStatus.WIFI;
+      case ConnectivityResult.none:
+        return ConnectivityStatus.Offline;
+        break;
+      default:
+    }
+  }
+}
