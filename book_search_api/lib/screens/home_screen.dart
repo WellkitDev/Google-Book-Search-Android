900d877854c946c10711bbbc95782873fb5cc29e
WellkitDp <82035540+WellkitDP@users.noreply.github.com>
2022-11-27 19:10:54 +0700

    Update home_screen.dart

--- book_search_api/lib/screens/home_screen.dart
+++ book_search_api/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:book_search_api/provider/gtc.dart';
+import 'package:book_search_api/widgets/title_app.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -30,7 +31,7 @@ class _HomeScreenState extends State<HomeScreen> {
         displacement: 80,
         child: Column(
           children: [
-            //yitle
+            TitleApp(),
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
