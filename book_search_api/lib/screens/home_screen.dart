cedd9bc051e421dbdfd4ac76ec724feaed2f5149
WellkitDp <82035540+WellkitDP@users.noreply.github.com>
2022-11-27 18:42:05 +0700

    Create home_screen.dart

---
+++ book_search_api/lib/screens/home_screen.dart
@@ -0,0 +1,60 @@
+import 'package:book_search_api/provider/gtc.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  static const routeName = '/home';
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    getBooksData();
+  }
+
+  Future<void> getBooksData() async {
+    await Provider.of<GTC>(context, listen: false).getCategoryList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: RefreshIndicator(
+        color: Color(0xff0DB067),
+        backgroundColor: Colors.white,
+        displacement: 80,
+        child: Column(
+          children: [
+            //yitle
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      //categoriselection
+                      child: Column(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        onRefresh: () async {
+          await getBooksData();
+        },
+      ),
+      //floatinglocation
+      //floatingbbutton
+    );
+  }
+}
