2135b8395d5c5d310ef84226c01ad57ae33432b0
WellkitDp <82035540+WellkitDev@users.noreply.github.com>
2022-12-16 02:50:12 +0700

    Update bookshelf_screen.dart

--- book_search_api/lib/screens/bookshelf_screen.dart
+++ book_search_api/lib/screens/bookshelf_screen.dart
@@ -1,6 +1,9 @@
+import 'package:book_search_api/provider/books.dart';
 import 'package:book_search_api/provider/bookshelf.dart';
 import 'package:book_search_api/widgets/navbar.dart';
+import 'package:book_search_api/widgets/saved_book.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 class BookShelfScreen extends StatefulWidget {
@@ -24,7 +27,63 @@ class _BookShelfScreenState extends State<BookShelfScreen> {
             return Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Material(),
+                Material(
+                  elevation: 2,
+                  child: Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage("images/book-title.jpg"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Container(
+                      color: Colors.black38,
+                      child: SafeArea(
+                        child: Padding(
+                          padding: EdgeInsets.only(
+                              left: 16.0, right: 16, top: 16.0, bottom: 0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Bookshelf',
+                                textAlign: TextAlign.end,
+                                style: GoogleFonts.montserrat(
+                                  textStyle: TextStyle(
+                                    fontSize: 36,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: FutureBuilder(
+                    future: Provider.of<Bookshelf>(context, listen: false)
+                        .fetchAndSetBooks(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<dynamic> snapshot) {
+                      return snapshot.connectionState == ConnectionState.waiting
+                          ? Center(
+                              child: CircularProgressIndicator(),
+                            )
+                          : bookshelf.savedBooks.length <= 0
+                              ? EmptyBookshelfWidget()
+                              : ListView.builder(
+                                  itemBuilder: (ctx, i) => SavedBookItem(
+                                    bookshelf.savedBooks.reversed.toList()[i],
+                                  ),
+                                );
+                    },
+                  ),
+                ),
               ],
             );
           },
@@ -34,6 +93,33 @@ class _BookShelfScreenState extends State<BookShelfScreen> {
             return Future.delayed(Duration.zero);
           });
         },
+      ),
+    );
+  }
+}
+
+class EmptyBookshelfWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: RichText(
+        text: TextSpan(
+          children: [
+            TextSpan(
+              text: "Clik",
+              style: TextStyle(color: Colors.black, fontSize: 15),
+            ),
+            WidgetSpan(
+                child: Icon(
+              Icons.bookmark_border,
+              size: 18,
+            )),
+            TextSpan(
+              text: "to add books to the bookshelf",
+              style: TextStyle(color: Colors.black, fontSize: 16),
+            ),
+          ],
+        ),
       ),
     );
   }
