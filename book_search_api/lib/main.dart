8f86c51b4c90a34cfef66f47e0d4caf8e2f4596b
WellkitDp <82035540+WellkitDev@users.noreply.github.com>
2022-12-16 02:50:10 +0700

    Update main.dart

--- book_search_api/lib/main.dart
+++ book_search_api/lib/main.dart
@@ -1,4 +1,13 @@
+import 'package:book_search_api/provider/books.dart';
+import 'package:book_search_api/provider/bookshelf.dart';
+import 'package:book_search_api/provider/categories.dart';
+import 'package:book_search_api/provider/gtc.dart';
+import 'package:book_search_api/screens/bookshelf_screen.dart';
 import 'package:book_search_api/screens/home_screen.dart';
+import 'package:book_search_api/screens/search_screen.dart';
+import 'package:book_search_api/screens/spesific_search_screen.dart';
+import 'package:book_search_api/services/connectivity_service.dart';
+import 'package:book_search_api/services/connectivity_status.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -14,15 +23,41 @@ class BooksApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return MaterialApp(
-      theme: ThemeData(
-        bottomSheetTheme: BottomSheetThemeData(
-          backgroundColor: Colors.black.withOpacity(0),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (BuildContext context) => Books(),
         ),
+        ChangeNotifierProvider(
+          create: (BuildContext context) => GTC(),
+        ),
+        ChangeNotifierProvider(
+          create: (BuildContext context) => Categories(),
+        ),
+        ChangeNotifierProvider(
+          create: (BuildContext context) => Bookshelf(),
+        ),
+        StreamProvider<ConnectivityStatus>(
+          create: (BuildContext context) =>
+              ConnectivityService().connectionStatusController.stream,
+        )
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          bottomSheetTheme: BottomSheetThemeData(
+            backgroundColor: Colors.black.withOpacity(0),
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+        title: 'Books Search Whit API ',
+        initialRoute: HomeScreen.routeName,
+        routes: {
+          HomeScreen.routeName: (context) => HomeScreen(),
+          SearchScreen.routeName: (context) => SearchScreen(),
+          BookShelfScreen.routeName: (context) => BookShelfScreen(),
+          SpecificSearchScreen.routeName: (context) => SpecificSearchScreen(),
+        },
       ),
-      debugShowCheckedModeBanner: false,
-      title: 'Search Books App',
-      home: HomeScreen(),
     );
   }
 }
